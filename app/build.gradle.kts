33e2d6fc601266ab4e91ae23e1a5917847fc612c
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2024-01-17 12:27:35 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.6"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
@@ -99,8 +99,8 @@ dependencies {
     implementation("com.maxkeppeler.sheets-compose-dialogs:clock:1.2.0")
 
     //Dagger hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    ksp("com.google.dagger:hilt-compiler:2.49")
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.hilt:hilt-common:1.1.0")
     ksp("androidx.hilt:hilt-compiler:1.1.0")
@@ -112,11 +112,11 @@ dependencies {
 
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     //Android
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
 
     //Kotlin
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
     //Realm
     id("io.realm.kotlin") version "1.13.0" apply false
 
     //Dagger Hilt
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 
     //GMS
     id("com.google.gms.google-services") version "4.4.0" apply false
@@ -19,7 +19,7 @@ plugins {
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 
     //Annotation Processing
-    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false //ksp
+    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false //ksp
 }
 
 tasks.register("clean", Delete::class) {
